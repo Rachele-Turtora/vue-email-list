bf6b75ba1730a15d09b609d2dc01458bbb1a8d81
Rachele-Turtora <racheleturtora21@gmail.com>
2024-05-28 14:37:59 +0200

    [UPD] print mails

--- js/main.js
+++ js/main.js
@@ -3,7 +3,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        mails: []
       }
+    },
+
+    created() {
+        for (let i = 0; i < 10; i++){
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((response) => {
+            this.mails.push(response.data)    
+        })
+        }
     }
   }).mount('#app')
